2629704bf610d6d1d297faac6e44445d8d39ca76
Fabio Mendes <fabiojmendes@gmail.com>
2022-03-07 15:30:48 -0500

    Fine tuning lualine and adding more mappings

--- lua/user/general.lua
+++ lua/user/general.lua
@@ -2,9 +2,20 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
-    component_separators = '|',
-    section_separators = '',
+  },
+  sections = {
+    lualine_b = {
+      'branch',
+      {
+        'diff',
+        symbols = {
+          added = ' ',
+          modified = ' ',
+          removed = ' ',
+        },
+      },
+      'diagnostics',
+    },
   },
 }
 
--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -24,6 +24,8 @@ wk.register {
   ['gl'] = { '<cmd>lua vim.diagnostic.open_float()<CR>', 'Line Diagnostic' },
   ['[d'] = { '<cmd>lua vim.diagnostic.goto_prev()<CR>', 'Prev Diagnostic' },
   [']d'] = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'Next Diagnostic' },
+  ['[g'] = { '<cmd>Gitsigns prev_hunk<CR>', 'Prev Git Diff' },
+  [']g'] = { '<cmd>Gitsigns next_hunk<CR>', 'Next Git Diff' },
 }
 
 -- Navigate buffers
@@ -53,20 +55,6 @@ keymap('v', 'p', '"_dP', opts)
 local M = {}
 
 M.on_attach = function(_, bufnr)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', [[<cmd>lua vim.lsp.buf.formatting_sync()<CR>]], opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting_sync()' ]]
   vim.api.nvim_set_keymap('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
