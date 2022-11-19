11103a7fcdfcb803fac3ec9f0e3b8f62b9010f9a
Fabio Mendes <fabiojmendes@gmail.com>
2022-11-18 22:11:15 -0500

    Fine tuning tree-sitter sintaxes

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,29 +6,7 @@ end
 -- Parsers must be installed manually via :TSInstall
 configs.setup({
   ensure_installed = {
-    'bash',
-    'c',
-    'cmake',
-    'cpp',
-    'diff',
-    'dockerfile',
-    'fish',
-    'go',
-    'java',
-    'javascript',
-    'json',
-    'json5',
-    'lua',
-    'make',
-    'markdown',
-    'python',
-    'regex',
-    'rust',
-    'sql',
-    'toml',
-    'typescript',
-    'vim',
-    'yaml',
+    'bash', 'fish', 'lua', 'python', 'yaml', 'json'
   },
   highlight = {
     enable = true, -- false will disable the whole extension
