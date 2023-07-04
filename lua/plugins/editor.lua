d5c5b77af7ff67e4f286de64475ad25b704876a1
Fabio Mendes <fabiojmendes@gmail.com>
2023-07-04 11:28:47 -0400

    Telescope fzf native

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -19,4 +19,14 @@ return {
       },
     },
   },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
 }
