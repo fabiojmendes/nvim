b9a384632f4337c52499a10b4c97afc81e04db90
Fabio Mendes <fabiojmendes@gmail.com>
2024-08-13 14:37:30 -0400

    Updating rust plugins

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,36 +1,13 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    "mrcjkb/rustaceanvim",
     opts = {
-      servers = {
-        -- Ensure mason installs the server
-        rust_analyzer = {
-          keys = {
-            { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
-            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
-          },
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-                loadOutDirsFromCheck = true,
-                runBuildScripts = true,
-              },
-              -- Add clippy lints for Rust.
-              checkOnSave = {
-                allFeatures = true,
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              },
-              procMacro = {
-                enable = true,
-                ignored = {
-                  ["async-trait"] = { "async_trait" },
-                  ["napi-derive"] = { "napi" },
-                  ["async-recursion"] = { "async_recursion" },
-                },
-              },
+      server = {
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            files = {
+              excludeDirs = { ".embuild" },
             },
           },
         },
@@ -41,21 +18,63 @@ return {
       vim.api.nvim_set_hl(0, "@lsp.type.string.rust", {})
     end,
   },
-
-  {
-    "simrat39/rust-tools.nvim",
-    opts = {
-      tools = {
-        inlay_hints = {
-          auto = false,
-        },
-      },
-    },
-  },
-
-  {
-    "Saecki/crates.nvim",
-    event = { "BufRead Cargo.toml" },
-    config = true,
-  },
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     opts = {
+  --       servers = {
+  --         -- Ensure mason installs the server
+  --         rust_analyzer = {
+  --           keys = {
+  --             { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+  --             { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+  --             { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+  --           },
+  --           settings = {
+  --             -- ["rust-analyzer"] = {
+  --             --   cargo = {
+  --             --     allFeatures = true,
+  --             --     loadOutDirsFromCheck = true,
+  --             --     runBuildScripts = true,
+  --             --   },
+  --             --   -- Add clippy lints for Rust.
+  --             --   checkOnSave = {
+  --             --     allFeatures = true,
+  --             --     command = "clippy",
+  --             --     extraArgs = { "--no-deps" },
+  --             --   },
+  --             --   procMacro = {
+  --             --     enable = true,
+  --             --     ignored = {
+  --             --       ["async-trait"] = { "async_trait" },
+  --             --       ["napi-derive"] = { "napi" },
+  --             --       ["async-recursion"] = { "async_recursion" },
+  --             --     },
+  --             --   },
+  --             -- },
+  --           },
+  --         },
+  --       },
+  --     },
+  --     -- Disable semantic tokens for rust strings tree-sitter injections will work
+  --     init = function()
+  --       vim.api.nvim_set_hl(0, "@lsp.type.string.rust", {})
+  --     end,
+  --   },
+  --
+  --   {
+  --     "simrat39/rust-tools.nvim",
+  --     opts = {
+  --       tools = {
+  --         inlay_hints = {
+  --           auto = false,
+  --         },
+  --       },
+  --     },
+  --   },
+  --
+  --   {
+  --     "Saecki/crates.nvim",
+  --     event = { "BufRead Cargo.toml" },
+  --     config = true,
+  --   },
 }
