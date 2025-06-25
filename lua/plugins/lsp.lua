1ef6077dddb90881813b420a84a0ed6995e339ae
Fabio Mendes <fabiojmendes@gmail.com>
2025-06-25 15:59:58 -0400

    Remove config related to ltex-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,59 +16,8 @@ return {
       inlay_hints = {
         enabled = false,
       },
-      servers = {
-        ltex = {
-          mason = false,
-          keys = {
-            {
-              "K",
-              function()
-                local bufnr = vim.api.nvim_get_current_buf()
-                if vim.lsp.get_clients({ bufnr = bufnr, name = "ltex" }) then
-                  require("user.dictionary").word_under_cursor()
-                else
-                  vim.lsp.hover()
-                end
-              end,
-            },
-          },
-        },
-      },
-      setup = {
-        ltex = function(_, opts)
-          local lang = "en-US"
-          -- opts.autostart = false
-          opts.root_dir = require("lazyvim.util").root.get()
-          opts.settings = {
-            ltex = {
-              language = lang,
-              dictionary = {
-                [lang] = {},
-              },
-              markdown = {
-                nodes = {
-                  ["Link"] = "dummy",
-                },
-              },
-              languageToolHttpServerUri = vim.env.LANGUAGE_TOOL_HTTP_SERVER_URI,
-              additionalRules = {
-                enablePickyRules = true,
-              },
-            },
-          }
-          opts.on_init = function(client)
-            local spell_file_name = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
-            local spell_file = io.open(spell_file_name, "r")
-            if spell_file then
-              local dict = client.config.settings.ltex.dictionary[lang]
-              for line in spell_file:lines() do
-                table.insert(dict, line)
-              end
-              spell_file:close()
-            end
-          end
-        end,
-      },
+      servers = {},
+      setup = {},
     },
   },
 
