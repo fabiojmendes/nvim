d6b237fd1ada71092717abfc8d978dc8b24c0f55
Fabio Mendes <fabiojmendes@gmail.com>
2025-09-18 13:41:32 -0400

    Update to lazyvim 15

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,21 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-if vim.fn.executable("gitui") == 1 then
-  -- gitui instead of lazygit
-  vim.keymap.set("n", "<leader>gg", function()
-    require("lazyvim.util").terminal.open({ "gitui" })
-  end, { desc = "gitui (cwd)" })
-  vim.keymap.set("n", "<leader>gG", function()
-    require("lazyvim.util").terminal.open({ "gitui" }, { cwd = require("lazyvim.util").root.get() })
-  end, { desc = "gitui (root dir)" })
-end
-
 -- Use gl for diagnostics
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 -- Maps <C-c> to <Esc> so it triggers abbreviations
 vim.keymap.set("i", "<c-c>", "<esc>", { desc = "Escape" })
-
--- Map mac keys to alt-j & alt-k
-vim.cmd("map ∆ <M-j>")
-vim.cmd("map ˚ <M-k>")
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "echasnovski/mini.surround",
+    "nvim-mini/mini.surround",
     opts = {
       -- Module mappings. Use `''` (empty string) to disable one.
       mappings = {
